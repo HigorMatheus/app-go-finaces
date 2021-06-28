98e3dc209d59e47cae55b71f59fea4a9b5bc604b
Higor Matheus <hmoliveira83@gmail.com>
2021-06-27 21:04:19 -0300

    criando cardis values

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { HighlightCard } from '../../Components/HighlightCard';
+
+import {
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+  Icon,
+  HighlightCards,
+} from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/53712358?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Higor</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
+    </Container>
+  );
+}
